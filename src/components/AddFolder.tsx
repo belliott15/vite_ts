347fbdc39fc6f6488aca4a0c07d437f83a4ec92e
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2024-03-25 21:50:14 -0700

    trying to get modal to work and updated read me

--- src/components/AddFolder.tsx
+++ src/components/AddFolder.tsx
@@ -8,7 +8,7 @@ interface ModalProps {
   closeModal: () => void;
 }
 
-const AddFolder = ({ children, visible = true, closeModal }: ModalProps) => {
+const AddFolder = ({ children, visible = false, closeModal }: ModalProps) => {
   const [title, setTitle] = useState();
 
   const handleSubmit = (e: string) => {
@@ -18,12 +18,15 @@ const AddFolder = ({ children, visible = true, closeModal }: ModalProps) => {
   };
 
   return (
-    <div>
-      <dialog open={visible}>
-        <h1>{children}</h1>
+    <div className="">
+      <dialog
+        open={visible}
+        className=" bg-emerald-950/60 border-2 border-emerald-950 rounded p-2"
+      >
         <button onClick={closeModal}>
           <FontAwesomeIcon icon={faX} />
         </button>
+        <h1>{children}</h1>
         <form>
           <input value={title} placeholder="title" />
           <button onClick={(e) => handleSubmit(e)}>Submit</button>
